220d47f549d3bc81e1dd5f46cdaf5b57408a2f22
mithisar brahma <moznony@gmail.com>
2026-04-12 14:50:41 +0530

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="nav">
     <div class="nav-inner">
-      <div class="logo">🃏 Hazari</div>
+      <div class="logo">🃏</div>
       <div class="links">
         <router-link to="/">Home</router-link>
         <router-link to="/about">About</router-link>
